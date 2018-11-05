b85ec5ae86323228e2dc7f26deba14d483c5bb1c
abukhleif <moh.noor94@gmail.com>
2018-11-05 02:42:33 +0200

    Add 'common_elements_in_two_sorted_arrays' problem

---
+++ src/main/python/common_elements_in_two_sorted_arrays.py
@@ -0,0 +1,53 @@
+"""
+    Common Elements in Two Sorted Arrays
+
+    Write a function that returns the common elements (as an array)
+    between two sorted arrays of integers (ascending order).
+
+    Example: The common elements between [1, 3, 4, 6, 7, 9] and [1, 2, 4, 5, 9, 10] are [1, 4, 9].
+
+    - More details: https://www.udemy.com/11-essential-coding-interview-questions/
+"""
+
+
+def common_elements_solution1(list1, list2):
+    """
+    Time:   O(max(n, m)), n,m: sizes of list1, list2.
+    Space:  O(n)
+    """
+    result = []
+    list1set = set(list1)
+    for n in list2:
+        if n in list1set:
+            result.append(n)
+    return result
+
+
+def common_elements_solution2(list1, list2):
+    """
+    Time:   O(max(n, m)), n,m: sizes of list1, list2.
+    Space:  O(n)
+    """
+    result = []
+    i = j = 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] == list2[j]:
+            result.append(list1[i])
+            i += 1
+            j += 1
+        elif list1[i] > list2[j]:
+            j += 1
+        else:
+            i += 1
+
+    return result
+
+
+if __name__ == '__main__':
+    print(common_elements_solution1([1, 3, 4, 6, 7, 9], [1, 2, 4, 5, 9, 10]))
+    print(common_elements_solution1([1, 2, 9, 10, 11, 12], [0, 1, 2, 3, 4, 5, 8, 9, 10, 12, 14, 15]))
+    print(common_elements_solution1([0, 1, 2, 3, 4, 5], [6, 7, 8, 9, 10, 11]))
+    print()
+    print(common_elements_solution2([1, 3, 4, 6, 7, 9], [1, 2, 4, 5, 9, 10]))
+    print(common_elements_solution2([1, 2, 9, 10, 11, 12], [0, 1, 2, 3, 4, 5, 8, 9, 10, 12, 14, 15]))
+    print(common_elements_solution2([0, 1, 2, 3, 4, 5], [6, 7, 8, 9, 10, 11]))
