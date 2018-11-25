909491e066070e13f735a318b7405ff5b2450c4e
Mohammad Noor <moh.noor94@gmail.com>
2018-11-25 16:59:11 +0200

    Add 'LinearSearch' and 'BinarySearch'

---
+++ src/main/java/search/BinarySearch.java
@@ -0,0 +1,59 @@
+package search;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 5, 7, 9, 12, 15};
+
+        System.out.println(binarySearch(ints, 3));
+        System.out.println(binarySearch(ints, 8));
+        System.out.println(binarySearch(ints, 9));
+        System.out.println(binarySearch(ints, 15));
+        System.out.println(binarySearch(ints, 1));
+
+        System.out.println();
+
+        System.out.println(IterativeBinarySearch(ints, 3));
+        System.out.println(IterativeBinarySearch(ints, 8));
+        System.out.println(IterativeBinarySearch(ints, 9));
+        System.out.println(IterativeBinarySearch(ints, 15));
+        System.out.println(IterativeBinarySearch(ints, 1));
+    }
+
+    /**
+     * Time:    O(log(n));  n: size of the array
+     * Space:   O(1)
+     */
+    public static int binarySearch(int[] array, int goal) {
+        return binarySearchHelper(array, 0, array.length - 1, goal);
+    }
+
+    private static int binarySearchHelper(int[] array, int startIndex, int endIndex, int goal) {
+        if (startIndex > endIndex) return -1;
+
+        int middleIndex = (startIndex + endIndex) / 2;
+
+        if (array[middleIndex] == goal) return middleIndex;
+        if (array[middleIndex] < goal) return binarySearchHelper(array, middleIndex + 1, endIndex, goal);
+        return binarySearchHelper(array, startIndex, middleIndex - 1, goal);
+    }
+
+    /**
+     * Time:    O(log(n));  n: size of the array
+     * Space:   O(1)
+     */
+    private static int IterativeBinarySearch(int[] array, int goal) {
+        int startIndex = 0;
+        int endIndex = array.length - 1;
+
+        while (endIndex >= startIndex) {
+            int middleIndex = (endIndex + startIndex) / 2;
+
+            if (array[middleIndex] == goal) return middleIndex;
+            else if (array[middleIndex] < goal) startIndex = middleIndex + 1;
+            else endIndex = middleIndex - 1;
+        }
+
+        return -1;
+    }
+}
---
+++ src/main/java/search/LinearSearch.java
@@ -0,0 +1,24 @@
+package search;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+        int[] ints = {1, 5, 9, 2, 3, 7, 4};
+
+        System.out.println(linearSearch(ints, 3));
+        System.out.println(linearSearch(ints, 8));
+    }
+
+    /**
+     * Time:    O(n)
+     * Space:   O(1); no additional space required
+     * ***
+     * If the array is not sorted, we cannot do better than O(n) search time.
+     */
+    public static int linearSearch(int[] array, int goal) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == goal) return i;
+        }
+        return -1;
+    }
+}
