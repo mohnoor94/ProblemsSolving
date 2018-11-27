b7b18b915e05bbc7e298b32f83f06d4f7c20f2af
Mohammad Noor <moh.noor94@gmail.com>
2018-11-27 16:25:51 +0200

    Add 'MinimalTree' problem and 'TreeNode' util class

---
+++ src/main/java/MinimalTree.java
@@ -0,0 +1,40 @@
+import util.tree.TreeNode;
+
+/**
+ * Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search
+ * tree with minimal height.
+ * ***
+ * - Cracking the coding interview, a book by Gayle Mcdowell (6th ed., q 4.4)
+ */
+public class MinimalTree {
+
+    public static void main(String[] args) {
+        int[] array1 = {1, 3, 5, 7, 9, 12};
+        int[] array2 = {1, 2, 4, 6, 9, 12, 15};
+        int[] array3 = {5};
+
+        TreeNode treeNode1 = buildMinimalBinarySearchTree(array1);
+        TreeNode treeNode2 = buildMinimalBinarySearchTree(array2);
+        TreeNode treeNode3 = buildMinimalBinarySearchTree(array3);
+
+        treeNode1.print();
+        treeNode2.print();
+        treeNode3.print();
+    }
+
+    private static TreeNode buildMinimalBinarySearchTree(int[] array) {
+        return getMiddleTreeNode(array, 0, array.length - 1);
+    }
+
+    private static TreeNode getMiddleTreeNode(int[] array, int start, int end) {
+        if (start > end) return null;
+
+        int mid = (start + end) / 2;
+
+        TreeNode node = new TreeNode(array[mid]);
+        node.setLeft(getMiddleTreeNode(array, start, mid - 1));
+        node.setRight(getMiddleTreeNode(array, mid + 1, end));
+
+        return node;
+    }
+}
---
+++ src/main/java/util/tree/TreeNode.java
@@ -0,0 +1,42 @@
+package util.tree;
+
+public class TreeNode {
+
+    private int data;
+    private TreeNode left, right;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public void print() {
+        inorderPrint();
+        System.out.println();
+    }
+
+    private void inorderPrint() {
+        if (left != null) left.inorderPrint();
+        System.out.print(data + ", ");
+        if (right != null) right.inorderPrint();
+    }
+}
