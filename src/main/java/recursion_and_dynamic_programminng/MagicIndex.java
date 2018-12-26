72bf180c0649a8c63bc92bfbf73b24a51c2aea05
AbuKhleif <moh.noor94@gmail.com>
2018-12-26 23:39:29 +0200

    Add 'BitOperations' problem description

--- src/main/java/recursion_and_dynamic_programminng/MagicIndex.java
+++ src/main/java/recursion_and_dynamic_programminng/MagicIndex.java
@@ -1,5 +1,15 @@
 package recursion_and_dynamic_programminng;
 
+/**
+ * A magic index in an array A[0 ... n-1] is defined to be an index such that A[i] = i.
+ * Given a sorted array of distinct integers, write a method to find a magic index, if one exists,
+ * in array A.
+ * ***
+ * FOLLOW UP:
+ * What if the values are not distinct?
+ * ***
+ * - Cracking the coding interview, a book by Gayle Mcdowell (6th ed., q 8.3)
+ */
 public class MagicIndex {
 
     public static void main(String[] args) {
