f6970738c5f69bd442ad18f1fdb4f6e28e3aac6d
abukhleif <moh.noor94@gmail.com>
2019-07-05 03:34:21 +0300

    Add a better solution for 'odd_occurrence' problem

--- src/main/python/odd_occurrence.py
+++ src/main/python/odd_occurrence.py
@@ -7,6 +7,9 @@ def find_odd_occurred_number_sol1(nums):
     one. Find the odd occurring number.
 
     - More details: https://youtu.be/bMF2fG9eY0A
+
+    - Time: O(len(nums))
+    - Space: O(len(nums))
     """
     counter = Counter()
     for value in nums:
@@ -28,6 +31,9 @@ def find_odd_occurred_number_sol2(nums):
 
     - This solution takes less space.
     - More details: https://youtu.be/bMF2fG9eY0A
+
+    - Time: O(len(nums))
+    - Space: worst: O(len(nums))
     """
     values = set()
     for value in nums:
@@ -39,8 +45,26 @@ def find_odd_occurred_number_sol2(nums):
     return next(iter(values))
 
 
+def find_odd_occurred_number_sol3(nums):
+    """
+    You are given an array of repeating numbers. All numbers repeat in even way, except for
+    one. Find the odd occurring number.
+
+    - Time: O(len(nums))
+    - Space: O(1)
+    """
+    xor = 0
+
+    for n in nums:
+        xor ^= n
+
+    return xor
+
+
 if __name__ == '__main__':
     print(find_odd_occurred_number_sol1((1, 2, 3, 1, 3)),
-          find_odd_occurred_number_sol2((1, 2, 3, 1, 3)))
+          find_odd_occurred_number_sol2((1, 2, 3, 1, 3)),
+          find_odd_occurred_number_sol3((1, 2, 3, 1, 3)))
     print(find_odd_occurred_number_sol1((1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 6, 4, 3, 2, 1)),
-          find_odd_occurred_number_sol2((1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 6, 4, 3, 2, 1)))
+          find_odd_occurred_number_sol2((1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 6, 4, 3, 2, 1)),
+          find_odd_occurred_number_sol3((1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 8, 7, 6, 4, 3, 2, 1)))
